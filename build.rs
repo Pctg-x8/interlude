5b65c0385af534045df27a739561a36d5539b032
S.Percentage <Syn.Tri.Naga@gmail.com>
2017-05-11 14:25:00 +0900

    fixed build script

--- build.rs
+++ build.rs
@@ -1,8 +1,11 @@
 fn main()
 {
-	// Adding Vulkan Library Path
-	let mut vksdk_base = std::env::var("VULKAN_SDK").map(std::path::PathBuf::from)
-		.expect("required Vulkan SDK Base Path as $Env:VULKAN_SDK");
-	vksdk_base.push("Lib");
-	println!("cargo:rustc-link-search={}", vksdk_base.to_string_lossy());
+	if cfg!(windows)
+	{
+		// Adding Vulkan Library Path
+		let mut vksdk_base = std::env::var("VULKAN_SDK").map(std::path::PathBuf::from)
+			.expect("required Vulkan SDK Base Path as $Env:VULKAN_SDK");
+		vksdk_base.push("Lib");
+		println!("cargo:rustc-link-search={}", vksdk_base.to_string_lossy());
+	}
 }
