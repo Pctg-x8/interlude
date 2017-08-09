ab9a762d8f3398567def07c96e6e27f6d0d9a143
S.Percentage <Syn.Tri.Naga@gmail.com>
2017-08-09 18:53:39 +0900

    update make_extension.sh to append module export code into lib.rs

--- vk/defs/make_extension.sh
+++ vk/defs/make_extension.sh
@@ -7,9 +7,17 @@ extension_name=$2
 spec_version=$3
 spec_hdr=VK_${extension_class^^}
 output_path=src/${extension_name}_${extension_class}.rs
+libroot_path=src/lib.rs
 
 echo "//! ${spec_hdr}_$extension_name extensions" >> $output_path
 echo "" >> $output_path
 echo "pub const ${spec_hdr}_${extension_name^^}_SPEC_VERSION: usize = $spec_version;" >> $output_path
 echo "pub static ${spec_hdr}_${extension_name^^}_EXTENSION_NAME: &'static str = \"${spec_hdr}_${extension_name}\";" >> $output_path
 echo "" >> $output_path
+
+# module registration into libroot
+echo "" >> $libroot_path
+echo "#[cfg(feature = \"${spec_hdr}_$extension_name\")]" >> $libroot_path
+echo "mod ${extension_name}_${extension_class};" >> $libroot_path
+echo "#[cfg(feature = \"${spec_hdr}_$extension_name\")]" >> $libroot_path
+echo "pub use ${extension_name}_${extension_class}::*;" >> $libroot_path
