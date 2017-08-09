096bb1aac95e1bdb84a0d70d173a35b271ba8ef1
S.Percentage <Syn.Tri.Naga@gmail.com>
2017-08-09 18:36:43 +0900

    update make_extension.sh to output to file directly

--- vk/defs/make_extension.sh
+++ vk/defs/make_extension.sh
@@ -6,11 +6,10 @@ extension_class=$1
 extension_name=$2
 spec_version=$3
 spec_hdr=VK_${extension_class^^}
+output_path=src/${extension_name}_${extension_class}.rs
 
-echo "//! ${spec_hdr}_$extension_name extensions"
-echo ""
-echo "pub const ${spec_hdr}_${extension_name^^}_SPEC_VERSION: usize = $spec_version;"
-echo "pub static ${spec_hdr}_${extension_name^^}_EXTENSION_NAME: &'static str = \"${spec_hdr}_${extension_name}\";";
-echo ""
-echo "use libc::*;"
-echo "use super::*;"
+echo "//! ${spec_hdr}_$extension_name extensions" >> $output_path
+echo "" >> $output_path
+echo "pub const ${spec_hdr}_${extension_name^^}_SPEC_VERSION: usize = $spec_version;" >> $output_path
+echo "pub static ${spec_hdr}_${extension_name^^}_EXTENSION_NAME: &'static str = \"${spec_hdr}_${extension_name}\";" >> $output_path
+echo "" >> $output_path
