98c8bcbe69ae68c622dffbddbc0751f2508750eb
S.Percentage <Syn.Tri.Naga@gmail.com>
2017-08-16 12:47:43 +0900

    fixed example

--- examples/triangle.rs
+++ examples/triangle.rs
@@ -1,16 +1,20 @@
 
 extern crate interlude;
 use interlude::*;
-use interlude::ffi::*;
 
-fn main()
+const VERTEX_FORMAT: Format = Format::Component(32, PackedPixelOrder::RGBA, FormatType::Float);
+
+fn main() { game().or_crash(); }
+fn game() -> EngineResult<()>
 {
+	let color_subres = ImageSubresourceRange { aspect: ImageAspect::Color.into(), .. Default::default() };
+
 	let engine = EngineBuilder::<EmptyInput>::new("com.cterm2.interlude.examples.triangle".into(), (0, 1, 0), "Rendering Triangle".into(), &Size2(640, 480))
-		.asset_base(std::env::current_dir().unwrap().into()).launch().or_crash();
+		.asset_base(std::env::current_dir().unwrap().into()).launch()?;
 
 	// make buffer and staging data
 	let bp = BufferPreallocator::new(&engine, &[BufferContent::Vertex(std::mem::size_of::<[[CVector4; 2]; 3]>())]);
-	let (dev, stg) = bp.instantiate().or_crash();
+	let (dev, stg) = bp.instantiate()?;
 	stg.map().map(|m|
 	{
 		*m.map_mut::<[[CVector4; 2]; 3]>(bp.offset(0)) = [
@@ -18,52 +22,46 @@ fn main()
 			[[0.375, 0.25, 0.5, 1.0], [1.0, 0.0, 0.0, 1.0]],
 			[[-0.375, 0.25, 0.5, 1.0], [0.0, 1.0, 1.0, 1.0]]
 		];
-	}).or_crash();
+	})?;
 
 	// make Framebuffer
-	let &Size2(w, h) = engine.render_window().size();
-	let vport = Viewport::from(engine.render_window().size().clone());
-	let fb = engine.render_window().render_targets().iter().map(|v| Framebuffer::new_for_presented(&engine, v, Some(true), &Size3(w, h, 1)))
-		.collect::<Result<Vec<_>, _>>().or_crash();
+	let vport = Viewport::from(engine.render_window().size());
+	let fb = engine.render_window().render_targets().iter().map(|v| Framebuffer::new_for_presented(&engine, v, Some(true), engine.render_window().size()))
+		.collect::<EngineResult<Vec<_>>>()?;
 
 	// load shaders and build pipeline state
 	let vshader = VertexShader::from_asset(&engine, "examples.triangle.vert", "main",
 		&[VertexBinding::PerVertex(std::mem::size_of::<[CVector4; 2]>() as u32)],
-		&[VertexAttribute(0, VkFormat::R32G32B32A32_SFLOAT, 0), VertexAttribute(0, VkFormat::R32G32B32A32_SFLOAT, std::mem::size_of::<CVector4>() as u32)]).or_crash();
-	let fshader = FragmentShader::from_asset(&engine, "engine.shaders.TrivialFragment", "main").or_crash();
-	let psl = PipelineLayout::new(&engine, &[], &[]).or_crash();
+		&[VertexAttribute(0, VERTEX_FORMAT, 0), VertexAttribute(0, VERTEX_FORMAT, std::mem::size_of::<CVector4>() as u32)])?;
+	let fshader = FragmentShader::from_asset(&engine, "engine.shaders.TrivialFragment", "main")?;
+	let psl = PipelineLayout::new(&engine, &[], &[])?;
 	let ps_mold = GraphicsPipelineBuilder::new(&psl, PreciseRenderPass(fb[0].renderpass(), 0))
 		.primitive_topology(PrimitiveTopology::TriangleList(false))
 		.vertex_shader(PipelineShaderProgram::unspecialized(&vshader))
 		.fragment_shader(PipelineShaderProgram::unspecialized(&fshader))
 		.viewport_scissors(&[ViewportWithScissorRect::default_scissor(&vport)])
 		.blend_state(&[AttachmentBlendState::Disabled]);
-	let ps = GraphicsPipelines::new(&engine, &[&ps_mold]).or_crash().pop().unwrap();
+	let ps = GraphicsPipelines::new(&engine, &[&ps_mold])?.pop().unwrap();
 
 	// Transfer data / Setting image layout
-	TransientTransferCommandBuffers::allocate(&engine, 1).and_then(|setup_commands|
 	{
 		let bmbarriers = [
-			BufferMemoryBarrier::hold_ownership(&stg, 0 .. bp.total_size(), 0, VK_ACCESS_TRANSFER_READ_BIT),
-			BufferMemoryBarrier::hold_ownership(&dev, 0 .. bp.total_size(), 0, VK_ACCESS_TRANSFER_WRITE_BIT)
+			BufferMemoryBarrier { buffer: &stg, range: 0 .. bp.total_size(), dst_access: AccessFlag::TransferRead.into(), .. Default::default() },
+			BufferMemoryBarrier { buffer: &dev, range: 0 .. bp.total_size(), dst_access: AccessFlag::TransferWrite.into(), .. Default::default() }
 		];
-		let bmbarrier_ret = BufferMemoryBarrier::hold_ownership(&dev, 0 .. bp.total_size(), VK_ACCESS_TRANSFER_WRITE_BIT, VK_ACCESS_VERTEX_ATTRIBUTE_READ_BIT);
-		let imbarriers = engine.render_window().render_targets().iter()
-			.map(|x| ImageMemoryBarrier::hold_ownership(x, ImageSubresourceRange::base_color(), 0, VK_ACCESS_MEMORY_READ_BIT, VkImageLayout::Undefined, VkImageLayout::PresentSrcKHR))
-			.collect::<Vec<_>>();
-		
-		try!(setup_commands.begin(0).and_then(|recorder| recorder
-			.pipeline_barrier(VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT, false, &[], &bmbarriers, &imbarriers)
+		ImmediateTransferCommandSubmission::begin(&engine)?
+			.pipeline_barrier_on(PipelineStage::Transfer, false, &[], &bmbarriers, &engine.render_window().render_targets().iter()
+				.map(|x| ImageMemoryBarrier::initialize_undef(x, color_subres.clone(), AccessFlag::MemoryRead.into(), ImageLayout::PresentSrc))
+				.collect::<Vec<_>>())
 			.copy_buffer(&stg, &dev, &[BufferCopyRegion(0, 0, bp.total_size())])
-			.pipeline_barrier(VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT, false, &[], &[bmbarrier_ret], &[])
-		.end()));
-		setup_commands.execute()
-	}).or_crash();
+			.pipeline_barrier_on(PipelineStage::Transfer, false, &[],
+				&[BufferMemoryBarrier { src_access: AccessFlag::VertexAttributeRead.into(), .. bmbarriers[1].clone() }.flip()], &[]);
+	}
 
 	// Forward Presenting(For Intel Graphics)
 	/*info!("Forward Presenting...");
 	{
-		let ordersem = engine.create_queue_fence().or_crash();
+		let ordersem = engine.create_queue_fence()?;
 		wframe.acquire_next_backbuffer_index(&ordersem).and_then(|index|
 		{
 			engine.allocate_transient_graphics_command_buffers(1).and_then(|fp_commands|
@@ -76,33 +74,37 @@ fn main()
 				));
 				fp_commands.execute(Some((&ordersem, VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT)))
 			}).map(|_| index)
-		}).and_then(|index| wframe.present(engine.graphics_queue_ref(), index, None)).or_crash();
-		engine.wait_device().or_crash();
+		}).and_then(|index| wframe.present(engine.graphics_queue_ref(), index, None))?;
+		engine.wait_device()?;
 	}*/
 
 	// Draw commands and submit it
-	let cb = GraphicsCommandBuffers::allocate(&engine, engine.render_window().render_targets().len()).or_crash();
-	for (n, recorder) in cb.begin_all().or_crash()
+	let cb = GraphicsCommandBuffers::new(&engine, engine.render_window().render_targets().len())?;
+	for (n, recorder) in cb.begin_all().enumerate()
 	{
-		let clear_value = AttachmentClearValue::Color(0.0, 0.0, 0.0, 1.0);
-		let imbarrier_rt = ImageMemoryBarrier::hold_ownership(&engine.render_window().render_targets()[n], ImageSubresourceRange::base_color(),
-			VK_ACCESS_MEMORY_READ_BIT, VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT, VkImageLayout::PresentSrcKHR, VkImageLayout::ColorAttachmentOptimal);
-		recorder
-			.pipeline_barrier(VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT, VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT, false, &[], &[], &[imbarrier_rt])
-			.begin_render_pass(&fb[n], &[clear_value], false)
+		recorder?
+			.pipeline_barrier_on(PipelineStage::ColorAttachmentOutput, false, &[], &[], &[
+				ImageMemoryBarrier
+				{
+					image: &engine.render_window().render_targets()[n], subresource_range: color_subres.clone(),
+					src_access: AccessFlag::MemoryRead.into(), dst_access: AccessFlag::ColorAttachmentWrite.into(),
+					src_layout: ImageLayout::PresentSrc, dst_layout: ImageLayout::ColorAttachmentOptimal, .. Default::default()
+				}
+			])
+			.begin_render_pass(&fb[n], &[AttachmentClearValue::Color(0.0, 0.0, 0.0, 1.0)], false)
 			.bind_pipeline(&ps)
 			.bind_vertex_buffers(&[(&dev, bp.offset(0))])
 			.draw(3, 1)
 			.end_render_pass()
-		.end().or_crash();
+		.end()?;
 	}
 
-	let ordersem = QueueFence::new(&engine).or_crash();
-	let render_completion = QueueFence::new(&engine).or_crash();
+	let ordersem = QueueFence::new(&engine)?;
+	let render_completion = QueueFence::new(&engine)?;
 	engine.render_window().acquire_next_target_index(&ordersem).and_then(|index|
-		engine.submit_graphics_commands(&[cb[index as usize]], &[(&ordersem, VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT)], Some(&render_completion), None).map(|_| index)
-	).and_then(|index| engine.render_window().present(&engine, index, Some(&render_completion)).map(|_| index)).or_crash();
+		engine.submit_graphics_commands(&[cb[index as usize]], &[(&ordersem, &PipelineStage::ColorAttachmentOutput)], Some(&render_completion), None).map(|_| index)
+	).and_then(|index| engine.render_window().present(&engine, index, Some(&render_completion)).map(|_| index))?;
 
 	engine.process_all_messages();
-	engine.wait_device().or_crash();
+	engine.wait_device()
 }
